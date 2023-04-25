bcf36427094c084f78b95dadc7262bb981ca9936
Jaimemilio <jaemlo@gmail.com>
2023-04-25 11:34:30 +0200

    some cleanup

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -19,7 +19,7 @@ public class UIManager : MonoBehaviour
         player.Coordinates.x = Input.location.lastData.latitude;
         player.Coordinates.y = Input.location.lastData.longitude;
         player.time = Input.location.lastData.timestamp;
-        PlayerLocationText.text = player.Coordinates.ToString();
+        //PlayerLocationText.text = player.Coordinates.ToString();
        // ClosestStationText.text = StationFinder.instance.ClosestStation.StationName;
         //CurrentWeatherText.text = WeatherData.instance.Info.currently.summary;
         CurrentWeatherText.text = world.currentWeather.ToString();
