1f71315e674d2bfdbad87b395f1af602d24e118b
saltopepper <42365602+saltopepper@users.noreply.github.com>
2023-04-26 17:33:38 +0200

    FightingCharacter

---
+++ Assets/FightingCharacter.cs
@@ -0,0 +1,56 @@
+    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightingCharacter : MonoBehaviour
+{
+    Vector2 startPos;
+    Vector2 endPos;
+    float tapDistance = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            switch (touch.phase)
+            {
+                // Record initial touch position.
+                case TouchPhase.Began:
+                    startPos = touch.position;
+                    break;
+
+                case TouchPhase.Moved:
+                    break;
+
+                // Report that a direction has been chosen when the finger is lifted.
+                case TouchPhase.Ended:
+                    endPos = touch.position;
+                    if (Vector2.Distance(startPos, endPos) < tapDistance)
+                    {
+                        //Tap
+                        Debug.Log(Camera.main.WorldToScreenPoint(touch.position).x);
+                        Debug.Log(Camera.main.scaledPixelWidth);
+                    }
+                    else
+                    {
+                        //Swipe
+
+                    }
+                    break;
+
+                //Report that touch was held
+                case TouchPhase.Stationary:
+
+                    break;
+            }
+        }
+    }
+}
