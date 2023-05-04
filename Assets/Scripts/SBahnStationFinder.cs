9a4b5e3e60ce754f824e141998773907334e3258
saltopepper <42365602+saltopepper@users.noreply.github.com>
2023-05-04 08:10:54 +0200

    kontrolleur fix, and startup stuff

--- Assets/Scripts/SBahnStationFinder.cs
+++ Assets/Scripts/SBahnStationFinder.cs
@@ -58,13 +58,13 @@ public class SBahnStationFinder : MonoBehaviour {
         Debug.Log("We got station data");
 		Info = JsonConvert.DeserializeObject<StationInfo>(www.downloadHandler.text);
 		//Info = JsonUtility.FromJson<StationInfo>(www.downloadHandler.text);
-		Debug.Log(www.downloadHandler.text);
+		//Debug.Log(www.downloadHandler.text);
 		StationFinder.instance.clearStations();
 		foreach (PlaceDetails pd in Info.results)
 		{
 
 			StationFinder.instance.addStation(new StationData(pd.name, pd.geometry.location.lat, pd.geometry.location.lng));
-			Debug.Log(pd.geometry.location.ToString());
+			Debug.Log(pd.name.ToString());
 		}
 
 		yield return new WaitForSeconds(0);
